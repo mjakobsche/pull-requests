5b9b27b4976f19b9bed48de9a878cc50114fc041
Mateusz Jakobsche <mateusz.jakobsche@buddy.works>
2024-04-22 10:38:41 +0200

    script

---
+++ pr.sh
@@ -0,0 +1,20 @@
+BASE_BRANCH="${1}"
+AMOUNT="${2}"
+CHANGED_FILE="changes.txt"
+COUNTER=0
+
+while [ $COUNTER -lt $AMOUNT ]
+do
+    CHANGE=$(date)
+    PR_BRANCH="${BASE_BRANCH}-${COUNTER}"
+    git checkout $BASE_BRANCH
+    git pull
+    sleep 3
+    git branch "${PR_BRANCH}"    
+    git checkout "${PR_BRANCH}"    
+    echo "${CHANGE}" > "${CHANGED_FILE}"
+    git add "${CHANGED_FILE}"
+    git commit -am "iteration ${COUNTER}"
+    git push -u origin "${PR_BRANCH}"
+    gh pr create --title "pr no. ($COUNTER)" --body "appended ${CHANGE} to ${CHANGED_FILE}"
+done
