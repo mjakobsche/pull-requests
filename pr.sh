28e0762b557234a2f28f1710315a04c1a483b8db
Mateusz Jakobsche <mateusz.jakobsche@buddy.works>
2024-04-22 11:17:03 +0200

    sc

--- pr.sh
+++ pr.sh
@@ -1,11 +1,11 @@
-AMOUNT=$1
-COUNTER=1
 BASE_BRANCH="$(git branch --show-current)"
+ITERATION=$(expr $(git branch | grep "${BASE_BRANCH}" | sed -e "s/[^0-9]//g" | sort -n | tail -n 1) + 1)
+AMOUNT=$(expr ${1} + ${ITERATION})
 CHANGED_FILE="changes.txt"
-while [ ${COUNTER} -lt ${AMOUNT} ]
+while [ ${ITERATION} -lt ${AMOUNT} ]
 do
     CHANGE=$(date)
-    PR_BRANCH="${BASE_BRANCH}-${COUNTER}"
+    PR_BRANCH="${BASE_BRANCH}-${ITERATION}"
     git checkout "${BASE_BRANCH}"
     git pull
     sleep 3
@@ -13,8 +13,8 @@ do
     git checkout "${PR_BRANCH}"    
     echo "${CHANGE}" > "${CHANGED_FILE}"
     git add "${CHANGED_FILE}"
-    git commit -am "iteration ${COUNTER}"
+    git commit -am "iteration ${ITERATION}"
     git push -u origin "${PR_BRANCH}"
-    gh pr create --title "pr no. (${COUNTER})" --body "appended ${CHANGE} to ${CHANGED_FILE}"
+    gh pr create --title "pr no. (${ITERATION})" --body "appended ${CHANGE} to ${CHANGED_FILE}"
     ((COUNTER++))
 done
